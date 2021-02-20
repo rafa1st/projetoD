1a87d6dbf1227d35cf6bed8806cee967b771f2ad
Rafael <rafa1st@hotmail.com>
2021-02-20 01:19:18 -0300

    update teste

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 function Home() {
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Clique para aumentar o contador!!!</h1>
             <Contador />
             <div>teste</div>
         </div>
